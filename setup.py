37babbfa6ddcd67801eab5f2857543ebc57acdd5
Shaun Chiang <redbeansodapop@hotmail.com>
2017-07-14 16:39:39 +0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above
+  name = 'sentence-sem-sim',
+  packages = ['sentence-sem-sim'], # this must be the same as the name above
   version = '0.1',
-  description = 'A random test lib',
-  author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  description = 'Sentence Semantic Similarity in Python.',
+  author = 'Shaun Chiang',
+  author_email = 'redbeansodapop@hotmail.com',
+  url = 'https://github.com/shaun-chiang/sentence-sem-sim/tree/master', # use the URL to the github repo
+  download_url = 'https://github.com/shaun-chiang/sentence-sem-sim/archive/0.2.tar.gz', # I'll explain this in a second
+  keywords = ['sentence', 'semantic', 'similarity'], # arbitrary keywords
   classifiers = [],
 )
