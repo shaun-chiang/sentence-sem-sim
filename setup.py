782840573db30878f44f132f4e5398f573f76e33
Shaun Chiang <redbeansodapop@hotmail.com>
2017-07-15 00:14:53 +0800

    Update setup info

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 setup(
   name = 'sentence-sem-sim',
   packages = ['sentence-sem-sim'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.5',
   description = 'Sentence Semantic Similarity in Python.',
   author = 'Shaun Chiang',
   author_email = 'redbeansodapop@hotmail.com',
   url = 'https://github.com/shaun-chiang/sentence-sem-sim/tree/master', # use the URL to the github repo
-  download_url = 'https://github.com/shaun-chiang/sentence-sem-sim/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['sentence', 'semantic', 'similarity'], # arbitrary keywords
   classifiers = [],
   license='MIT',
   install_requires=['nltk>=3.2.1',
-                    'numpy>=1.11']
+                    'numpy>=1.11'],
+  python_requires='>=3',
 )
